54bf723aa605adc493c53f0ecd880cf0a266aec2
= <=>
2024-04-13 16:34:59 -0400

    incorporated usage of utility runtime() & removed imports

--- deleter.py
+++ deleter.py
@@ -2,25 +2,12 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from Chrome_Session import chromeSession
-from time import time
-from datetime import datetime
-
+from util.utilities import runtime
 if __name__ == "__main__":
-    session = chromeSession(12730876)
-    session.start()
-    session.move_container(300, 'veCG00255', 'dz-F-OB-10')
+    session = chromeSession('hdc3', 12730876)
     containers = [
-
+        
     ]
-    qty = len(containers)
     for csX in containers:
-        start_time = time()
-        session.sideline_delete(csX)
-        elapsed_time = time() - start_time
-        if elapsed_time < 60:
-            print(f"{datetime.now()} // Runtime: {elapsed_time:.2f} seconds // {csX} DELETED\n{'-' * 47}\n")
-        elif elapsed_time >= 60:
-            minutes = int(elapsed_time // 60)
-            seconds = int(elapsed_time % 60)
-            print(f"Runtime: {minutes}m:{seconds}s")
+        print(f'{csX} // {runtime(session.sideline_delete, csX, "TRASH")}')
     session.close()
