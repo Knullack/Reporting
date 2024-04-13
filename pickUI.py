d921529fa42b67842ab7bd8c66deff5e81350d21
= <=>
2024-04-13 16:35:59 -0400

    utility.runtime usage & removed imports

--- pickUI.py
+++ pickUI.py
@@ -2,19 +2,13 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from Chrome_Session import chromeSession
-from time import time
-from datetime import datetime
+from util.utilities import runtime
+
 
 if __name__ == "__main__":
     session = chromeSession('hdc3',12730876)
     for i in range(1000):
-        start_time = time()
-        csX = session.pickUI('veCG00003', 'paXCG00003')
-        elapsed_time = time() - start_time
-        if elapsed_time < 60:
-            print(f"{datetime.now()} // Runtime: {elapsed_time:.2f} seconds // {csX} DELETED\n{'-' * 47}\n")
-        elif elapsed_time >= 60:
-            minutes = int(elapsed_time // 60)
-            seconds = int(elapsed_time % 60)
-            print(f"Runtime: {minutes}m:{seconds}s")
+        time, csX = runtime(session.pickUI, vehicle='veCG00201', cage='paXCG00201', dirtyVehicle_dz_location='veCG00245', deleted_container_to_dz='TRASH')
+        print(f' {csX}\n{'-' * 47}\n')
+        # print(runtime(session.PAWS_tradional_picking))
     session.close()
