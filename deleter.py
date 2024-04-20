1bb304bae92ca3e26eafa8cbfc793391e971bb8f
= <=>
2024-04-20 15:40:52 -0400

    debug editing

--- deleter.py
+++ deleter.py
@@ -5,19 +5,19 @@ from Chrome_Session import chromeSession
 from util.utilities import runtime
 if __name__ == "__main__":
     session = chromeSession('hdc3', 12730876)
-    containers = [
-        
-    ]
-#     containers = {
-# 'csXPJDMTSX8' : 'ZZS0OOWKHZ'
-#     }
+    # containers = [
+    #     'csXDel'
+    # ]
+    containers = {
+'tsX10d5jmzd' : 'ZZUPTN13GX'
+    }
     # for i, csX in enumerate(containers, start=1):
     #     print(f'{i}) {csX} // {runtime(session.unbindHierarchy, csX)}')
     # for i, csX in enumerate(containers, start=1):
     #     print(f'{i}) {csX} // {runtime(session.sideline_delete, csX, 'TRASH')}')
-    for i, csX in enumerate(containers, start=1):
-        print(f'{i}/{len(containers)}) {csX} // {runtime(session.deleteItem, csX ,'container')}')
-    # for location, item in containers.items():
-        # print(f'{location}: {item}\n{runtime(session.deleteItem, location, 'single', item)}')
+    # for i, csX in enumerate(containers, start=1):
+    #     print(f'{i}/{len(containers)}) {csX} // {runtime(session.deleteItem, csX ,'container')}')
+    for location, item in containers.items():
+        print(f'{location}: {item}\n{runtime(session.deleteItem, location, 'single', item)}')
 
     session.close()
