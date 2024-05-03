4ccb9b272bf07526bc896aa4d773e6ebccec7052
= <=>
2024-05-03 17:07:12 -0400

    edits

--- deleter.py
+++ deleter.py
@@ -6,17 +6,23 @@ from util.utilities import runtime
 if __name__ == "__main__":
     session = chromeSession('hdc3', 12730876)
     # containers = [
-    #     'csXDel'
+        
     # ]
     containers = {
-'tsX10d5jmzd' : 'ZZUPTN13GX'
+
     }
     # for i, csX in enumerate(containers, start=1):
     #     print(f'{i}) {csX} // {runtime(session.unbindHierarchy, csX)}')
+    
     # for i, csX in enumerate(containers, start=1):
     #     print(f'{i}) {csX} // {runtime(session.sideline_delete, csX, 'TRASH')}')
+
     # for i, csX in enumerate(containers, start=1):
-    #     print(f'{i}/{len(containers)}) {csX} // {runtime(session.deleteItem, csX ,'container')}')
+    #     print(f'{i}/{len(containers)}) {csX} // {runtime(session.sideline_delete, csX ,'TRASH')}')
+
+    # for i, csX in enumerate(containers, start=1):
+    #     print(f'{i}/{len(containers)}) {csX} // {runtime(session.move_container, 200 , csX,'TRASH')}')
+
     for location, item in containers.items():
         print(f'{location}: {item}\n{runtime(session.deleteItem, location, 'single', item)}')
 
