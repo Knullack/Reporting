db391dc9b0e181759341dd64e0fe3ad527d40abe
= <=>
2024-05-11 09:22:43 -0400

    used file, edited for purpose

--- deleter.py
+++ deleter.py
@@ -9,7 +9,19 @@ if __name__ == "__main__":
         
     # ]
     containers = {
-
+'tsX0fw04wjm',
+'csXz6PT2CRC',
+'csXz5PT2CRC',
+'csXTz0Ma8DI',
+'csXRlHg3K1t',
+'csXPJHT3QG8',
+'csXPJFVBG5F',
+'csXPJF6R9C5',
+'csXPHNPCVRR',
+'csXPHM7MRLC',
+'csXKv48YVkA',
+'csXGjKRGmJV',
+'csX9KYS7Kzs'
     }
     # for i, csX in enumerate(containers, start=1):
     #     print(f'{i}) {csX} // {runtime(session.unbindHierarchy, csX)}')
@@ -17,13 +29,13 @@ if __name__ == "__main__":
     # for i, csX in enumerate(containers, start=1):
     #     print(f'{i}) {csX} // {runtime(session.sideline_delete, csX, 'TRASH')}')
 
-    # for i, csX in enumerate(containers, start=1):
-    #     print(f'{i}/{len(containers)}) {csX} // {runtime(session.sideline_delete, csX ,'TRASH')}')
+    for i, csX in enumerate(containers, start=1):
+        print(f'{i}/{len(containers)}) {csX} // {runtime(session.sideline_delete, csX ,'TRASH')}')
 
     # for i, csX in enumerate(containers, start=1):
     #     print(f'{i}/{len(containers)}) {csX} // {runtime(session.move_container, 200 , csX,'TRASH')}')
 
-    for location, item in containers.items():
-        print(f'{location}: {item}\n{runtime(session.deleteItem, location, 'single', item)}')
+    # for location, item in containers.items():
+    #     print(f'{location}: {item}\n{runtime(session.deleteItem, location, 'single', item)}')
 
     session.close()
