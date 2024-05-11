8624c20e7ccd24eef95b93fef0162e74a9a04c47
= <=>
2024-05-11 17:07:59 -0400

    execution changes

--- deleter.py
+++ deleter.py
@@ -1,41 +1,45 @@
 import sys
 import os
+from typing import Literal
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from Chrome_Session import chromeSession
 from util.utilities import runtime
 if __name__ == "__main__":
     session = chromeSession('hdc3', 12730876)
-    # containers = [
-        
-    # ]
-    containers = {
-'tsX0fw04wjm',
-'csXz6PT2CRC',
-'csXz5PT2CRC',
-'csXTz0Ma8DI',
-'csXRlHg3K1t',
-'csXPJHT3QG8',
-'csXPJFVBG5F',
-'csXPJF6R9C5',
-'csXPHNPCVRR',
-'csXPHM7MRLC',
-'csXKv48YVkA',
-'csXGjKRGmJV',
-'csX9KYS7Kzs'
+    list_containers: list = [
+'csXPHMTRMZ9',
+'csXPHRT8RBD',
+'csXGUz1DULr',
+'csXPH29TVY2',
+'csXPH29TVVK',
+'csXPH29TW26',
+'csXPH29TW3M',
+'csXPHVX6H4S',
+'csXGJ01txU4'
+
+    ]
+    dict_containers: dict = {
+
     }
-    # for i, csX in enumerate(containers, start=1):
-    #     print(f'{i}) {csX} // {runtime(session.unbindHierarchy, csX)}')
+
+    def unbind():
+        for i, csX in enumerate(list_containers, start=1):
+            print(f'{i}) {csX} // {runtime(session.unbindHierarchy, csX)}')
     
-    # for i, csX in enumerate(containers, start=1):
-    #     print(f'{i}) {csX} // {runtime(session.sideline_delete, csX, 'TRASH')}')
+    def sideline():
+        for i, csX in enumerate(list_containers, start=1):
+            print(f'{i}/{len(list_containers)}) {csX} // {runtime(session.sideline_delete, csX ,'TRASH')}')
 
-    for i, csX in enumerate(containers, start=1):
-        print(f'{i}/{len(containers)}) {csX} // {runtime(session.sideline_delete, csX ,'TRASH')}')
+    def moveContainer(destination: str):
+        for i, csX in enumerate(list_containers, start=1):
+            print(f'{i}/{len(list_containers)}) {csX} // {runtime(session.move_container, 200 , csX, destination)}')
 
-    # for i, csX in enumerate(containers, start=1):
-    #     print(f'{i}/{len(containers)}) {csX} // {runtime(session.move_container, 200 , csX,'TRASH')}')
+    def deleteItem(mode: Literal['container', 'single']):
+        for i, container in enumerate(list_containers):
+            print(f'{i}/{len(list_containers)} // {container} // {runtime(session.deleteItem, container, mode)}')
 
-    # for location, item in containers.items():
-    #     print(f'{location}: {item}\n{runtime(session.deleteItem, location, 'single', item)}')
 
+    # deleteItem('container')
+    sideline()
+    moveContainer('TRASH')
     session.close()
