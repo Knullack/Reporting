2dff0e20d511d216a1ffd9056f45b9ef8645b79c
= <=>
2024-05-31 17:05:22 -0400

    small edit

--- rodeo_delete.py
+++ rodeo_delete.py
@@ -12,5 +12,5 @@ if __name__ == "__main__":
     }
 
     for i, (sku, container) in enumerate(containers.items(), start=1):
-        print(f'{i}/{len(containers)} {container} :: {sku} // {runtime(session.rodeo_delete, container, sku)}')
+        print(f'\n{i}/{len(containers)}) {container} :: {sku} // {runtime(session.rodeo_delete, container, sku)}\n')
     session.close()
